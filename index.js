60f04f4970a6b284b9864c1f5aaa00cb2bd591bd
MR Masum <mrmasum820@gmail.com>
2023-11-26 17:52:00 +0600

    added dotenv

--- index.js
+++ index.js
@@ -9,7 +9,9 @@ const port = process.env.PORT || 5000
 app.use(cors())
 app.use(express.json())
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.lzjopzw.mongodb.net/?retryWrites=true&w=majority`
+// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.lzjopzw.mongodb.net/?retryWrites=true&w=majority`
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.o2pce7q.mongodb.net/?retryWrites=true&w=majority`
+console.log(uri);
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
